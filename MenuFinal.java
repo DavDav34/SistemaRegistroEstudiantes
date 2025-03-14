8631b5d20216eb675c197d41125a821667f98320
Ramírez Barrera David <david.allbella@gmail.com>
2025-03-13 23:44:52 -0600

    Menu Final

---
+++ MenuFinal.java
@@ -0,0 +1,47 @@
+public class SistemaRegistroEstudiantes {
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+        Estudiante[] estudiantes = new Estudiante[100];
+        int numEstudiantes = 0;
+        int opcion;
+
+        do {
+            System.out.println("\n--- Sistema de Registro de Estudiantes ---");
+            System.out.println("1. Registrar estudiante");
+            System.out.println("2. Mostrar estudiantes");
+            System.out.println("3. Buscar estudiante por matrícula");
+            System.out.println("4. Eliminar estudiante");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                  
+                    break;
+
+                case 2:
+                	
+                    break;
+
+                case 3:
+                  
+                    break;
+
+                case 4:
+                  
+                    break;
+
+                case 0:
+                
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 0);
+
+        scanner.close();
+    }
+}
